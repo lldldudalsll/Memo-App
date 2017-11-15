32e5068501f3a21e7a64c5ecaf4fc4a8ace8bd03
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-15 20:54:31 +0900

    modify plugins naming in webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -58,6 +58,6 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin()
     ]
 };
