f00b8a36441c03f749fa5608fccc1de7043ba1a0
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-25 06:28:13 +0900

    create memoList.js components

---
+++ src/components/MemoList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Memo from './Memo';
+import PropTypes from 'prop-types';
+
+class MemoList extends Component {
+    render() {
+
+        const mapToComponents = data => {
+            return data.map((memo, i) => {
+                    return (<Memo
+                                data={memo}
+                                ownership={ (memo.writer === this.props.currentUser) }
+                                key={memo._id}
+                            />);
+            });
+        };
+
+        return (
+            <div>
+                {/* <Memo/> */}
+                {mapToComponents(this.props.data)}
+            </div>
+        );
+    }
+}
+
+MemoList.propTypes = {
+    data: PropTypes.array,
+    currentUser: PropTypes.string
+}
+
+MemoList.defaultProps = {
+    data: [],
+    currentUser: ''
+}
+
+export default MemoList;
