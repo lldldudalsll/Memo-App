6d9a8d85de2c032c7fd6d508bf9c4b44f127d958
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-25 06:24:01 +0900

    add List action in memo reducer

--- src/reducers/memo.js
+++ src/reducers/memo.js
@@ -5,6 +5,13 @@ const initialState = {
     post: {
         status: 'INIT',
         error: -1
+    },
+    list: {
+        status: 'INIT',
+        data: [],
+        isLast: false
+        // isLast 값은 현재 로딩된 페이지가 마지막페이지인지 아닌지 알려주기 위함.
+        // 나중에 무한 스크롤링 기능 구현시 마지막 페이지에 도달하면 스크롤링이 멈추도록 해야 하므로
     }
 };
 
@@ -14,6 +21,7 @@ export default function memo(state, action) {
     }
 
     switch(action.type) {
+        // Post
         case types.MEMO_POST:
             return update(state, {
                 post: {
@@ -34,6 +42,32 @@ export default function memo(state, action) {
                     error: { $set: action.error }
                 }
             });
+        // List
+        case types.MEMO_LIST:
+            return update(state, {
+                list: {
+                    status: { $set: 'WAITING' },
+                }
+            });
+        case types.MEMO_LIST_SUCCESS:
+            if(action.isInitial){
+                return update(state, {
+                    list: {
+                        status: { $set: 'SUCCESS' },
+                        data: { $set: action.data },
+                        isLast: { $set: action.data.length < 6 }
+                    }
+                });
+            }
+            // loading older or newer memo
+            // to be implemented..
+            return state;
+        case types.MEMO_LIST_FAILURE:
+            return update(state, {
+                list: {
+                    status: { $set: 'FAILURE' }
+                }
+            })
         default:
             return state;
     }
