598159414adc7fcadb25175a01610524fd03659e
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-25 06:24:38 +0900

    add list action type

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,3 +1,5 @@
+/* 모든 action type 상수들을 모두 이 파일 안에 적어서 사용. */
+
 /* AUTHENTICATION */
 
 /* Login */
@@ -5,8 +7,6 @@ export const AUTH_LOGIN = "AUTH_LOGIN";
 export const AUTH_LOGIN_SUCCESS = "AUTH_LOGIN_SUCCESS";
 export const AUTH_LOGIN_FAILURE = "AUTH_LOGIN_FAILURE";
 
-// 우리는 모든 action type 상수들을 모두 이 파일 안에 적어서 사용 할 거에요.
-// 지금은 로그인 관련 Action type 만 있지만 앞으로 계속해서 추가해나갈거에요..
 
 /* Register */
 export const AUTH_REGISTER = "AUTH_REGISTER";
@@ -20,11 +20,17 @@ export const AUTH_GET_STATUS_FAILURE = "AUTH_GET_STATUS_FAILURE";
 
 /* Logout */
 export const AUTH_LOGOUT = "AUTH_LOGOUT";
-
-// 로그아웃은 성공하고 안하고가 중요하지 않기 떄문에, 액션 하나로도 충분합니다.
+// 로그아웃은 성공하고 안하고가 중요하지 않기 떄문에, 액션 하나로도 충분.
 
 
 /* MEMO */
+
+/* Post */
 export const MEMO_POST = "MEMO_POST";
 export const MEMO_POST_SUCCESS = "MEMO_POST_SUCCESS";
 export const MEMO_POST_FAILURE = "MEMO_POST_FAILURE";
+
+/* List */
+export const MEMO_LIST = "MEMO_LIST";
+export const MEMO_LIST_SUCCESS = "MEMO_LIST_SUCCESS";
+export const MEMO_LIST_FAILURE = "MEMO_LIST_FAILURE";
