85bd6504df6a7cf3273bea1ab76f5916ed6a3917
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-29 04:25:42 +0900

    edit memoRequest

--- src/actions/memo.js
+++ src/actions/memo.js
@@ -60,6 +60,14 @@ export function memoListRequest(isInitial, listType, id, username) {
         
         let url = '/api/memo';
 
+        if(typeof username === "undefined") {
+            // username not given, load public memo
+            url = isInitial ? url : `${url}/${listType}/${id}` // or url + '/' + listType + '/' +  id
+        } else {
+            // load memos of specific user
+            /* to be implemented */
+        }
+
         return axios.get(url)
         .then((response) => {
             dispatch(memoListSuccess(response.data, isInitial, listType));
