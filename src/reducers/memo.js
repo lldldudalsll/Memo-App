9533f2abba4e02793af96fbc4774b93cadc9dfeb
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-29 19:43:05 +0900

    add remove and edit reducer

--- src/reducers/memo.js
+++ src/reducers/memo.js
@@ -12,6 +12,14 @@ const initialState = {
         isLast: false
         // isLast 값은 현재 로딩된 페이지가 마지막페이지인지 아닌지 알려주기 위함.
         // 나중에 무한 스크롤링 기능 구현시 마지막 페이지에 도달하면 스크롤링이 멈추도록 해야 하므로
+    },
+    remove: {
+        status: 'INIT',
+        error: -1
+    },
+    edit: {
+        status: 'INIT',
+        error: -1,
     }
 };
 
@@ -20,7 +28,7 @@ export default function memo(state, action) {
         state = initialState;
     }
 
-    switch(action.type) {
+    switch(action.type) { // 예상되는 값을 반환하는 표현식
         // Post
         case types.MEMO_POST:
             return update(state, {
@@ -58,33 +66,87 @@ export default function memo(state, action) {
                         isLast: { $set: action.data.length < 6 }
                     }
                 });
-            } else {
-                if(action.isInitial === "new") {
-                    return update(state, {
-                        list: {
-                            status: { $set: 'SUCCESS'},
-                            data: { $unshift: action.data }
-                            // 배열의 앞 부분에 데이터를 추가할땐 $unshift 연산자를, 뒷 부분에 추가 할 땐 $push 연산자를 사용.
-                        }
-                    });
-                } else {
-                    return update(state, {
-                        list: {
-                            status: { $set: 'SUCCESS' },
-                            data: { $push: action.data },
-                            isLast: { $set: action.data.length < 6 }
-                        }
-                    });
-                }
             }
-            
-            return state;
+            if(action.listType === "new") {
+                return update(state, {
+                    list: {
+                        status: { $set: 'SUCCESS' },
+                        data: { $unshift: action.data }
+                        // 배열의 앞 부분에 데이터를 추가할땐 $unshift 연산자를, 뒷 부분에 추가 할 땐 $push 연산자를 사용.
+                    }
+                });
+            } 
+            return update(state, {
+                list: {
+                    status: { $set: 'SUCCESS' },
+                    data: { $push: action.data },
+                    isLast: { $set: action.data.length < 6 }
+                }
+            });
+                
         case types.MEMO_LIST_FAILURE:
             return update(state, {
                 list: {
                     status: { $set: 'FAILURE' }
                 }
             })
+
+        // Remove
+        case types.MEMO_REMOVE:
+            return update(state, {
+                remove: {
+                    status: { $set: 'WAITING' },
+                    error: { $set: -1 }
+                }
+            });
+        case types.MEMO_REMOVE_SUCCESS:
+            return update(state, {
+                remove: {
+                    status: { $set: 'SUCCESS' },
+                },
+                list: {
+                    data: { $splice: [[action.index, 1]]}
+                }
+            });
+        case types.MEMO_REMOVE_FAILURE:
+            return update(state, {
+                remove: {
+                    status: { $set: 'FAILURE' },
+                    error: { $set: action.error }
+                }
+            });
+
+        // Edit
+        case types.MEMO_EDIT:
+            return update(state, {
+                edit: {
+                    status: { $set: 'WAITING' },
+                    error: { $set: -1 },
+                    memo: { $set: undefined}
+                }
+            });
+
+        case types.MEMO_EDIT_SUCCESS:
+            return update(state, {
+                edit: {
+                    status: { $set: 'SUCCESS' },
+                },
+                list: {
+                    data: {
+                        [action.index]: { $set: action.memo }
+                        // list 의 데이터 중 [action.index] 번째 데이터를 새로운 데이터로 교체
+                    }
+                }
+            });
+
+        case types.MEMO_EDIT_FAILURE:
+            return update(state, {
+                edit: {
+                    status: { $set: 'FAILURE' },
+                    error: { $set: action.error }
+                }
+            });
+        
         default:
             return state;
     }
