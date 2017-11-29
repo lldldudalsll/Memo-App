90768b0539e506b661c0bea9b5f1520781bfddb4
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-11-29 19:46:53 +0900

    create remove, edit action

--- src/actions/memo.js
+++ src/actions/memo.js
@@ -4,7 +4,13 @@ import {
     MEMO_POST_FAILURE,
     MEMO_LIST,
     MEMO_LIST_SUCCESS,
-    MEMO_LIST_FAILURE
+    MEMO_LIST_FAILURE,
+    MEMO_REMOVE,
+    MEMO_REMOVE_SUCCESS,
+    MEMO_REMOVE_FAILURE,
+    MEMO_EDIT,
+    MEMO_EDIT_SUCCESS,
+    MEMO_EDIT_FAILURE
 } from './ActionTypes';
 
 import axios from 'axios';
@@ -18,7 +24,8 @@ export function memoPostRequest(contents) {
         return axios.post('/api/memo', { contents })
         .then((response) => {
             dispatch(memoPostSuccess());
-        }).catch((error) => {
+        })
+        .catch((error) => {
             dispatch(memoPostFailure(error.response.data.code));
         });
     };
@@ -71,7 +78,8 @@ export function memoListRequest(isInitial, listType, id, username) {
         return axios.get(url)
         .then((response) => {
             dispatch(memoListSuccess(response.data, isInitial, listType));
-        }).catch((error) => {
+        })
+        .catch((error) => {
             dispatch(memoListFailure());
         });
     };
@@ -96,4 +104,77 @@ export function memoListFailure() {
     return {
         type: MEMO_LIST_FAILURE
     };
+}
+
+/* MEMO REMOVE */
+export function memoRemoveRequest(id, index) {
+    return (dispatch) => {
+        dispatch(memoRemove());
+
+        return axios.delete('/api/memo/' + id)
+        .then((response) => {
+            // console.log(response);
+            dispatch(memoRemoveSuccess(index));
+        })
+        .catch((error) => {
+            // console.log(error);
+            dispatch(memoRemoveFailure(error.response.data.code));
+        });
+    };
+}
+
+export function memoRemove() {
+    return {
+        type: MEMO_REMOVE
+    };
+}
+
+export function memoRemoveSuccess(index) {
+    return {
+        type: MEMO_REMOVE_SUCCESS,
+        index
+    };
+}
+
+export function memoRemoveFailure(error) {
+    return {
+        type: MEMO_REMOVE_FAILURE,
+        error
+    };
+}
+
+/* MEMO EDIT */
+export function memoEditRequest(id, index, contents) {
+    return (dispatch) => {
+        dispatch(memoEdit());
+
+        return axios.put('api/memo/' + id, { contents })
+        .then((response) => {
+            dispatch(memoEditSuccess(index, response.data.memo));
+        })
+        .catch((error) => {
+            dispatch(memoEditFailure(error, response.data.code));
+        })
+    }
+}
+
+export function memoEdit() {
+    return {
+        type: MEMO_EDIT
+    }
+}
+
+export function memoEditSuccess(index, memo) {
+    return {
+        type: MEMO_EDIT_SUCCESS,
+        index,
+        memo
+    }
+}
+
+export function memoEditFailure(error) {
+    return {
+        type: MEMO_EDIT_FAILURE,
+        error
+    }
 }
