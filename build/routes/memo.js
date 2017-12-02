8dde8d7a9c4c9c2cefa4d9950cbe08adc820753e
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-12-02 10:10:11 +0900

    add http request user memo

--- build/routes/memo.js
+++ build/routes/memo.js
@@ -63,6 +63,70 @@ router.post('/', function (req, res) {
 });
 
 /*
+    TOGGLES STAR OF MEMO: POST /api/memo/star/:id
+*/
+
+router.post('/star/:id', function (req, res) {
+    // CHECK MEMO ID VALIDITY
+    if (!_mongoose2.default.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({
+            error: "INVALID ID",
+            code: 1
+        });
+    }
+    // CHECK LOGIN STATUS
+    if (typeof req.session.loginInfo === 'undefined') {
+        return res.status(403).json({
+            error: "NOT LOGGED IN",
+            code: 2
+        });
+    }
+    // FIND MEMO
+    _memo2.default.findById(req.params.id, function (err, memo) {
+        if (err) throw err;
+
+        // MEMO DOES NOT EXIST
+        if (!memo) {
+            return res.status(404).json({
+                error: "NO RESOURCE",
+                code: 3
+            });
+        }
+
+        // GET INDEX OF USERNAME IN THE ARRAY
+        var index = memo.starred.indexOf(req.session.loginInfo.username);
+
+        // CHECK WHETHER THE USER ALREADY HAS GIVEN A STAR
+        var hasStarred = index === -1 ? false : true;
+
+        if (!hasStarred) {
+            // IF IT DOES NOT EXIST
+            memo.starred.push(req.session.loginInfo.username);
+        } else {
+            // ALREADY starred
+            memo.starred.splice(index, 1);
+        }
+
+        // SAVE THE MEMO
+        memo.save(function (err, memo) {
+            if (err) throw err;
+            res.json({
+                success: true,
+                'has_starred': !hasStarred,
+                memo: memo
+            });
+        });
+
+        // 이 API 는 주어진 :id 값을 가진 메모를 찾고
+        // 해당 메모의 starred 데이터 배열에 자신의 아이디가 존재하지 않는다면
+        // 자신의 아이디를 starred 데이터 배열에 추가하고,
+        // 이미 존재한다면 starred 데이터 배열에서 제거합니다.
+        // API 실행이 완료되면 별을 주었는지 가져갔는지에 대한 값 
+        // has_star 과 새로운 메모데이터인 memo 객체를 반환합니다 (업데이트를 할 수 있게끔)
+    });
+});
+
+/*
     MODIFY MEMO: PUT /api/memo/:id
     BODY SAMPLE: { contents: "sample "}
 */
@@ -152,7 +216,7 @@ router.delete('/:id', function (req, res) {
     }
 
     // FIND MEMO AND CHECK FOR WRITER
-    _memo2.default.findById(res.params.id, function (err, memo) {
+    _memo2.default.findById(req.params.id, function (err, memo) {
         if (err) throw err;
 
         if (!memo) {
@@ -234,6 +298,58 @@ router.get('/:listType/:id', function (req, res) {
     } else {
         // GET OLDER MEMO
         _memo2.default.find({ _id: { $lt: objId } }).sort({ _id: -1 }).limit(6).exec(function (err, memos) {
+            if (err) throw err;
+            return res.json(memos);
+        });
+    }
+});
+
+/*
+    READ MEMO OF A USER: GET /api/memo/:username
+*/
+
+router.get('/:username', function (req, res) {
+    _memo2.default.find({ writer: req.params.username }).sort({ "_id": -1 }).limit(6).exec(function (err, memos) {
+        if (err) throw err;
+        res.json(memos);
+    });
+});
+
+/*
+    READ ADDITIONAL (OLD/NEW) MEMO OF A USER: GET /api/memo/:username/:listType/:id
+*/
+
+router.get('/:username/:listType/:id', function (req, res) {
+    var listType = req.params.listType;
+    var id = req.params.id;
+
+    // CHECK LIST TYPE VALIDITY
+    if (listType !== 'old' && listType !== 'new') {
+        return res.status(404).json({
+            error: "INVALID LISTTYPE",
+            code: 1
+        });
+    }
+
+    // CHECK MEMO ID VALIDITY
+    if (!_mongoose2.default.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            error: "INVALID ID",
+            code: 2
+        });
+    }
+
+    var objId = new _mongoose2.default.Types.ObjectId(req.params.id);
+
+    if (listType === "new") {
+        // GET NEWER MEMO
+        _memo2.default.find({ writer: req.params.username, _id: { $gt: objId } }).sort({ _id: -1 }).limit(6).exec(function (err, memos) {
+            if (err) throw err;
+            return res.json(memos);
+        });
+    } else {
+        // GET OLDER MEMO
+        _memo2.default.find({ writer: req.params.username, _id: { $lt: objId } }).sort({ _id: -1 }).limit(6).exec(function (err, memos) {
             if (err) throw err;
             return res.json(memos);
         });
