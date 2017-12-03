ef5d0f2fa6d081f78a8a227765a358d5c94e383b
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-12-03 18:30:48 +0900

    Edit babel-polyfill and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 
 module.exports = {
     entry: [
+        'babel-polyfill',
         './src/index.js',
         './src/style.css'
     ],
@@ -32,8 +33,23 @@ module.exports = {
     },
     resolve: {
         modules: [path.resolve(__dirname, "src"), "node_modules"]
-    }
+    },
     // 위 설정을 추가해주면 React 프로젝트의 루트디렉토리를 설정하여, 
     // 나중에 ./components 혹은 ../components 이렇게 접근해야 되는 디렉토리를 
     // 바로 components 로 접근 할 수 있게 해줍니다.
+    plugins:[
+        new webpack.DefinePlugin({
+          'process.env':{
+            'NODE_ENV': JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress:{
+            warnings: true
+          }
+        })
+    ]
+    // webpack 을 require 하고 plugins: 부분에 위 처럼 설정을 하면 
+    // 코드가 production 환경으로 컴파일됩니다 (일부 경고 출력 사라짐)
+    // 또한, 코드가 Uglify 되어 (불필요한 공백 제거) 코드의 용량이 줄어듭니다.
 };
