023e58234e1bae0330f3e93eef6add3f15adfd7c
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-12-03 18:34:05 +0900

    Add toggleSearch and Rendering search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Search } from 'components';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            search: false
+        }
+
+        this.toggleSearch = this.toggleSearch.bind(this);
+    }
+
+    toggleSearch() {
+        this.setState({
+            search: !this.state.search
+        })
+    }
+
     render(){
 
         const loginButton = (
@@ -22,27 +41,34 @@ class Header extends React.Component {
         );
 
         return (
-            <nav>
-              <div className="nav-wrapper darken-1">
-                <Link to="/" className="brand-logo center">MEMOPAD</Link>
-            
-                <ul>
-                    <li>
-                        {/* <Link to="/wall"> */}
-                        <a>
-                            <i className="material-icons">search</i>
-                        </a>
-                        {/* </Link> */}
-                    </li>
-                </ul>
-            
-                <div className="right">
-                  <ul>
-                    { this.props.isLoggedIn ? logoutButton : loginButton }
-                  </ul>
+            <div>
+                <nav>
+                <div className="nav-wrapper darken-1">
+                    <Link to="/" className="brand-logo center">MEMOPAD</Link>
+                
+                    <ul>
+                        <li>
+                            {/* <Link to="/wall"> */}
+                            <a onClick={this.toggleSearch}>
+                                <i className="material-icons">search</i>
+                            </a>
+                            {/* </Link> */}
+                        </li>
+                    </ul>
+                
+                    <div className="right">
+                    <ul>
+                        { this.props.isLoggedIn ? logoutButton : loginButton }
+                    </ul>
+                    </div>
                 </div>
-              </div>
-            </nav>
+                </nav>
+                <ReactCSSTransitionGroup transitionName="search" transitionEnterTimeout={300} transitionLeaveTimeout={300}>
+                    {this.state.search ? <Search onClose={this.toggleSearch} 
+                                                 onSearch={this.props.onSearch}
+                                                 usernames={this.props.usernames}/> : undefined}
+                </ReactCSSTransitionGroup>
+            </div>
         );
     }
 }
@@ -50,12 +76,14 @@ class Header extends React.Component {
 // optional
 Header.propTypes = {
     isLoggedIn: PropTypes.bool,
-    onLogout: PropTypes.func
+    onLogout: PropTypes.func,
+    usernames: PropTypes.array
 }
 
 Header.defaultProps = {
     isLoggedIn: false,
-    onLogout: () => { console.error("logout function not defined");}
+    onLogout: () => { console.error("logout function not defined");},
+    usernames: []
 }
 
 export default Header;
