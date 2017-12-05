8154415ad017b561ea52d864042deffff03ba58b
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-12-06 02:23:48 +0900

    test

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 
-// import WebpackDevServer from 'webpack-dev-server';
+import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
 
 import morgan from 'morgan'; // HTTP REQUEST LOGGER
@@ -15,7 +15,7 @@ import api from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
-// const devPort= 4000;
+const devPort= 4000;
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -70,14 +70,14 @@ app.listen(port, ()=>{
     console.log('Express is listening on port', port);
 });
 
-// if(process.env.NODE_ENV == 'development'){
-//     console.log('Server is running on development mode');
-//     const config = require('../webpack.dev.config');
-//     const compiler = webpack(config);
-//     const devServer = new WebpackDevServer(compiler, config.devServer);
-//     devServer.listen(
-//         devPort, () => {
-//             console.log('webpack-dev-server is listening on port', devPort);
-//         }
-//     );
-// }
+if(process.env.NODE_ENV == 'development'){
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
